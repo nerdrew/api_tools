90d4a717fd5130c28b0273796d865ac80b63f57d
Andrew Ryan Lazarus <nerdrew@gmail.com>
2014-12-07 23:22:05 -0800

    Update shoulda include

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,7 @@ $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'active_record'
 require 'api_tools'
 require 'api_tools_specs'
-require 'shoulda/matchers/integrations/rspec'
+require 'shoulda/matchers'
 
 PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..')).freeze
 Dir[File.join(PROJECT_ROOT, 'spec', 'support', '**', '*.rb')].each { |file| require(file) }
